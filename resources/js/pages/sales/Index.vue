4666fe0c7e57707702747d6fdc3d97692c416c14
dalinezequiel <dalinezequiel44@gmail.com>
2025-07-21 01:03:31 +0200

    add sales page

---
+++ resources/js/pages/sales/Index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import MemberTable from '@/components/additional/MemberTable.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Sales',
+        href: '/sales',
+    },
+];
+</script>
+
+<template>
+    <Head title="Sales" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <MemberTable></MemberTable>
+    </AppLayout>
+</template>
