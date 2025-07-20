1a392bd6115d6ef74d7ddfefc6ed716cb23d8c9b
dalinezequiel <dalinezequiel44@gmail.com>
2025-07-21 01:01:28 +0200

    add financial page

---
+++ resources/js/pages/financial/Index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import MemberTable from '@/components/additional/MemberTable.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Financial',
+        href: '/financial',
+    },
+];
+</script>
+
+<template>
+    <Head title="Financial" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <MemberTable></MemberTable>
+    </AppLayout>
+</template>
