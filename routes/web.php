38c6d64807381218100652f98612552dcff3b40a
dalinezequiel <dalinezequiel44@gmail.com>
2025-07-21 12:04:15 +0200

    feat: add route for sales

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\Registration\RegistrationController;
 use App\Http\Controllers\Financial\FinancialController;
+use App\Http\Controllers\Sales\SalesController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -15,6 +16,7 @@ Route::get('dashboard', function () {
 
 Route::resource('registrations', RegistrationController::class);
 Route::resource('financial', FinancialController::class);
+Route::resource('sales', SalesController::class);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
