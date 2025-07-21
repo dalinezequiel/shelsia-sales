73de5f35cff646eaaf6a28acf1e6cc3e6f1230ac
dalinezequiel <dalinezequiel44@gmail.com>
2025-07-21 20:46:25 +0200

    refactor(NavFooter component): change tag 'a' to component 'Link'

---
+++ resources/js/pages/parameter/Index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Parameters',
+        href: '/parameters',
+    },
+];
+</script>
+
+<template>
+    <Head title="Parameters" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+    </AppLayout>
+</template>
