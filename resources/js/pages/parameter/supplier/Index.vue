52f8096f9a6073f0c2ff7c16ed24ef84d709e3d4
dalinezequiel <dalinezequiel44@gmail.com>
2025-07-22 23:05:57 +0200

    refactor: add supplier index page

--- resources/js/pages/parameter/supplier/Index.vue
+++ resources/js/pages/parameter/supplier/Index.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import ParameterLayout from '@/pages/parameter/Layout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Suppliers',
+        href: '/parameters/Suppliers',
+    },
+];
+</script>
+
 <template>
-    <h1>I'm supplier index</h1>
+    <Head title="Suppliers" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <ParameterLayout>
+            <h1>I'm supplier index</h1>
+        </ParameterLayout>
+    </AppLayout>
 </template>
