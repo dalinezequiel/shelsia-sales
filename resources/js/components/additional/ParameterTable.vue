1528408eefd0fb80ecf7fa761cab62ce5ceb4d29
dalinezequiel <dalinezequiel44@gmail.com>
2025-07-28 23:40:12 +0200

    refactor: add ParameterTable component

--- resources/js/components/additional/ParameterTable.vue
+++ resources/js/components/additional/ParameterTable.vue
@@ -93,7 +93,7 @@ defineProps({
                 </div>
             </div>
         </div>
-        <div class="p-6 px-0 overflow-auto">
+        <div class="pt-6 pb-4 px-0 overflow-auto">
             <table class="w-full mt-4 text-left table-auto min-w-max">
                 <thead>
                     <tr>
@@ -123,6 +123,59 @@ defineProps({
                     </tr>
                 </thead>
                 <tbody>
+                    <tr>
+                        <td class="p-4 border-b border-blue-gray-50">
+                            <div class="flex items-center gap-3">
+                                <img src="https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg"
+                                    alt="John Michael"
+                                    class="relative inline-block h-9 w-9 !rounded-full object-cover object-center" />
+                                <div class="flex flex-col">
+                                    <p
+                                        class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                        John Michael
+                                    </p>
+                                    <p
+                                        class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900 opacity-70">
+                                        john@creative-tim.com
+                                    </p>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="p-4 border-b border-blue-gray-50">
+                            <div class="flex flex-col">
+                                <p
+                                    class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                                    Manager
+                                </p>
+                                <p
+                                    class="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900 opacity-70">
+                                    Organization
+                                </p>
+                            </div>
+                        </td>
+                        <td class="p-4 border-b border-blue-gray-50">
+                            <div class="w-max">
+                                <div
+                                    class="relative grid items-center px-2 py-1 font-sans text-xs font-bold text-green-900 uppercase rounded-md select-none whitespace-nowrap bg-green-500/20">
+                                    <span class="">online</span>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="p-4 border-b border-blue-gray-50">
+                            <button
+                                class="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                type="button">
+                                <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                        aria-hidden="true" class="w-4 h-4">
+                                        <path
+                                            d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z">
+                                        </path>
+                                    </svg>
+                                </span>
+                            </button>
+                        </td>
+                    </tr>
                     <tr>
                         <td class="p-4 border-b border-blue-gray-50">
                             <div class="flex items-center gap-3">
