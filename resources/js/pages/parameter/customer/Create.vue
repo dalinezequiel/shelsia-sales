55205c46bb5cdcf5bec79e5f8af7280fb382c0a8
dalinezequiel <dalinezequiel44@gmail.com>
2025-07-31 15:40:16 +0200

    refactor: add customer create page

--- resources/js/pages/parameter/customer/Create.vue
+++ resources/js/pages/parameter/customer/Create.vue
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { Switch } from '@/components/ui/switch';
 import AppLayout from '@/layouts/AppLayout.vue';
 import ParameterLayout from '@/pages/parameter/Layout.vue';
 import { type BreadcrumbItem } from '@/types';
@@ -18,6 +19,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 </script>
 
 <template>
+
     <Head title="Customer Parameters" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
@@ -26,12 +28,10 @@ const breadcrumbs: BreadcrumbItem[] = [
                 <div class="relative mx-4 mt-2 overflow-hidden rounded-none bg-white bg-clip-border text-gray-700">
                     <div class="mb-8 flex items-center justify-between gap-8">
                         <div>
-                            <h5 class="text-blue-gray-900 block font-sans text-xl leading-snug font-semibold tracking-normal antialiased">
-                                Customer registration
+                            <h5
+                                class="text-blue-gray-900 block font-sans text-xl leading-snug font-semibold tracking-normal antialiased">
+                                New Customer
                             </h5>
-                            <p class="mt-1 block font-sans text-base leading-relaxed font-normal text-gray-700 antialiased">
-                                See information about all customers
-                            </p>
                         </div>
                     </div>
                 </div>
@@ -39,20 +39,22 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <form class="space-y-6">
                         <div class="grid gap-2">
                             <Label for="name">Name</Label>
-                            <Input id="name" class="mt-1 block w-full" required autocomplete="name" placeholder="Full name" />
+                            <Input id="name" class="mt-1 block w-full" required autocomplete="name"
+                                placeholder="Full name" />
                             <InputError class="mt-2" />
                         </div>
 
                         <div class="grid gap-2">
-                            <Label for="email">Surname</Label>
-                            <Input id="email" type="email" class="mt-1 block w-full" required autocomplete="username" placeholder="Surname" />
+                            <Label for="surname">Surname</Label>
+                            <Input id="surname" class="mt-1 block w-full" required autocomplete="username"
+                                placeholder="Surname" />
                             <InputError class="mt-2" />
                         </div>
 
                         <div class="grid gap-2">
                             <Label for="gender">Gender</Label>
-                            <Select>
-                                <SelectTrigger class="w-auto">
+                            <Select id="gender">
+                                <SelectTrigger class="w-auto mt-1">
                                     <SelectValue placeholder="Select gender" />
                                 </SelectTrigger>
                                 <SelectContent>
@@ -63,28 +65,34 @@ const breadcrumbs: BreadcrumbItem[] = [
                             <InputError class="mt-2" />
                         </div>
 
-                        <div class="grid gap-2">
-                            <Label for="phone">Phone</Label>
-                            <Input id="phone" type="number" class="mt-1 block w-full" required autocomplete="username" placeholder="Phone" />
-                            <InputError class="mt-2" />
+                        <div class="grid lg:grid-cols-2 lg:gap-4 ">
+                            <div class="grid gap-2 mb-6 lg:mb-0">
+                                <Label for="phone">Phone</Label>
+                                <Input id="phone" type="number" class="mt-1 block w-full" required autocomplete="phone"
+                                    placeholder="Phone" />
+                                <InputError class="mt-2" />
+                            </div>
+
+                            <div class="grid gap-2">
+                                <Label for="email">Email address</Label>
+                                <Input id="email" type="email" class="mt-1 block w-full" required
+                                    autocomplete="username" placeholder="Email address" />
+                                <InputError class="mt-2" />
+                            </div>
                         </div>
 
-                        <div class="grid gap-2">
-                            <Label for="email">Email address</Label>
-                            <Input id="email" type="email" class="mt-1 block w-full" required autocomplete="username" placeholder="Email address" />
-                            <InputError class="mt-2" />
+                        <div class="flex items-center space-x-2 ">
+                            <Switch id="airplane-mode" />
+                            <Label for="airplane-mode">Customer status</Label>
                         </div>
 
-                        <div class="flex items-center gap-4">
-                            <Link href="" class="rounded bg-[#DCDBDA] px-2 py-1.5 hover:bg-[#D1D1D1]">Back</Link>
+                        <div class="flex items-center gap-4 mt-10">
+                            <Link href="/parameters/customers"
+                                class="rounded bg-[#E3E3E3] px-4 py-2 text-sm hover:bg-[#F2F2F2]">Back</Link>
                             <Button class="cursor-pointer">Save</Button>
 
-                            <Transition
-                                enter-active-class="transition ease-in-out"
-                                enter-from-class="opacity-0"
-                                leave-active-class="transition ease-in-out"
-                                leave-to-class="opacity-0"
-                            >
+                            <Transition enter-active-class="transition ease-in-out" enter-from-class="opacity-0"
+                                leave-active-class="transition ease-in-out" leave-to-class="opacity-0">
                                 <p class="text-sm text-neutral-600">Saved.</p>
                             </Transition>
                         </div>
