c6802809acd7c2c3fbb449ea5e7e8422cc9c5f2c
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-02 01:12:05 +0200

    refactor: add create_customers_table migration

--- database/migrations/2025_08_01_193200_create_customers_table.php
+++ database/migrations/2025_08_01_193200_create_customers_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('gender');
             $table->string('phone');
             $table->string('email')->unique();
+            $table->string('address');
             $table->boolean('is_active')->default(false);
             $table->timestamps();
         });
