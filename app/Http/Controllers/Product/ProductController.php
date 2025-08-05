05efcd1e8777c39414e83c1b94d016fc3b638aeb
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-05 23:39:09 +0200

    feat: implement destroy method

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -105,8 +105,9 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Product $product)
     {
-        //
+        $product -> delete();
+        return redirect()->route('products.index')->with('success', 'Deleted com sucesso!');
     }
 }
