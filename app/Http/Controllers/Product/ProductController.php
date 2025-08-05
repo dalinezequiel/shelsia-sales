407dca346188bf3b3dbe6596f3fc06dd9788c242
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-05 23:29:59 +0200

    feat: implement store method

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -31,7 +31,29 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'barcode' => 'required',
+            'description' => 'required'
+        ]);
+
+        Product::create([
+            'barcode' => $request -> barcode,
+            'description' => $request -> description,
+            'category' => $request -> category,
+            'unit' => $request -> unit,
+            'supplier' => $request -> supplier,
+            'purchase_price' => $request -> purchase_price,
+            'sale_price' => $request -> sale_price,
+            'validity' => $request -> validity,
+            'minimum_stock' => $request -> minimum_stock,
+            'maximum_stock' => $request -> maximum_stock,
+            'available_stock' => $request -> available_stock,
+            'location' => $request -> location,
+            'image' => $request -> image,
+            'is_active' => $request -> is_active
+        ]);
+
+        return redirect()->route('products.index')->with('success', 'Cadastrado com sucesso!');
     }
 
     /**
