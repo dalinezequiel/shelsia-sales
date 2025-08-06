feb8b72eafb547e266782ca9d511b7f86dba8fc9
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-07 01:58:18 +0200

    feat: implement show method

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -59,9 +59,9 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Product $product)
     {
-        //
+        return Inertia::render('product/Show', compact('product'));
     }
 
     /**
