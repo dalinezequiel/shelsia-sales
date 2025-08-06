0b8df5f4bac36c43166a96755caa67064cdfb3a1
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-06 11:20:15 +0200

    refactor: add financial create page

--- resources/js/pages/financial/Create.vue
+++ resources/js/pages/financial/Create.vue
@@ -194,13 +194,13 @@ const breadcrumbs: BreadcrumbItem[] = [
                             <div
                                 class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-900 dark:peer-checked:bg-blue-600">
                             </div>
-                            <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Product
+                            <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Account
                                 status</span>
                         </label>
                     </div>
 
                     <div class="flex items-center gap-2 mt-10">
-                        <Link :href="route('accounts.index')"
+                        <Link :href="route('financial.index')"
                             class="px-4 py-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-900 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
