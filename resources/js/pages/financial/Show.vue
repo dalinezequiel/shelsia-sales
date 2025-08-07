f3975f07fb5cca4dc20dbbfd3780c757c8299446
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-07 08:32:45 +0200

    refactor: add financial show page

--- resources/js/pages/financial/Show.vue
+++ resources/js/pages/financial/Show.vue
@@ -57,6 +57,12 @@ const breadcrumbs: BreadcrumbItem[] = [
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium text-gray-500">Document number</dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{
+                                props.financial.document_number }}
+                            </dd>
+                        </div>
+                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Category</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.financial.category }}
                             </dd>
