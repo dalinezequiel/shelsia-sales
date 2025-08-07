92f896358261ae5a834bd91c285beb6fd6e31083
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-07 10:15:39 +0200

    refactor: add dashboard page

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,8 +2,8 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 import AdminStat from '@/components/additional/AdminStat.vue';
+import Ranking from '@/components/additional/Ranking.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -20,10 +20,7 @@ const breadcrumbs: BreadcrumbItem[] = [
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
             <AdminStat />
-            <div
-                class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <PlaceholderPattern />
-            </div>
+            <Ranking />
         </div>
     </AppLayout>
 </template>
