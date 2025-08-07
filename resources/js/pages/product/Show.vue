9c8420dc97103d72e5f42fb3dfa93d2612427c1d
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-07 02:05:17 +0200

    refactor: add product show page

--- resources/js/pages/product/Show.vue
+++ resources/js/pages/product/Show.vue
@@ -65,6 +65,12 @@ const breadcrumbs: BreadcrumbItem[] = [
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.product.sale_price }}
                             </dd>
                         </div>
+                        <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                            <dt class="text-sm font-medium text-gray-500">Available stock</dt>
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{
+                                props.product.available_stock }}
+                            </dd>
+                        </div>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Supplier</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.product.supplier }}
