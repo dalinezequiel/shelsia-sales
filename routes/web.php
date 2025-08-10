00d750eca72b8a3bf2f5f876946d49d14a278003
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-11 00:52:43 +0200

    feat: set Analysis route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Inertia\Inertia;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Financial\FinancialController;
 use App\Http\Controllers\Sales\SalesController;
+use App\Http\Controllers\Analysis\AnalysisController;
 use App\Http\Controllers\Parameter\Customer\CustomerController;
 use App\Http\Controllers\Parameter\Supplier\SupplierController;
 use App\Http\Controllers\Parameter\Account\AccountController;
@@ -20,6 +21,7 @@ Route::get('dashboard', function () {
 Route::resource('products', ProductController::class);
 Route::resource('financial', FinancialController::class);
 Route::resource('sales', SalesController::class);
+Route::resource('analysis', AnalysisController::class);
 Route::prefix('parameters')->group(function(){
     Route::resource('/customers', CustomerController::class);
     Route::resource('/suppliers', SupplierController::class);
