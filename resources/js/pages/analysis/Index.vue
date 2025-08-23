88a5332fca754e08c10aea2e1cdec0638bcf7db4
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-23 13:26:25 +0200

    refactor: add analysis index page

--- resources/js/pages/analysis/Index.vue
+++ resources/js/pages/analysis/Index.vue
@@ -3,6 +3,12 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 
+defineProps({
+    forecasts: {
+        type: Object,
+        required: true
+    }
+});
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -24,6 +30,9 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="relative flex flex-col w-full h-full text-gray-700 bg-white shadow-md rounded-xl bg-clip-border">
             <div class="relative mx-4 mt-4 text-gray-700 bg-white rounded-none bg-clip-border">
                 <h1>Predictions Index</h1>
+                <p>
+                    {{ forecasts }}
+                </p>
             </div>
         </div>
     </AppLayout>
