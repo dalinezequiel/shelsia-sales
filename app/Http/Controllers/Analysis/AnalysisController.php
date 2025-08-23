3e425c126f7e18f04d16497eb9d58a2e7424d20e
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-23 13:23:33 +0200

    refactor: implement index method

--- app/Http/Controllers/Analysis/AnalysisController.php
+++ app/Http/Controllers/Analysis/AnalysisController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Analysis;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Inertia\Inertia;
 
 class AnalysisController extends Controller
@@ -13,7 +14,36 @@ class AnalysisController extends Controller
      */
     public function index()
     {
-        return Inertia::render('analysis/Index');
+        $forecast_endpoint = 'http://127.0.0.1:5000/preditions';
+        $sales = [
+            "periods" => 10,
+            "freq" => "d",
+            "data" => [
+                [
+                   "ds" => "2007-12-10",
+		 	        "y" => 9.59076113897809 
+                ],
+                [
+                    "ds" => "2007-12-11",
+		 	        "y" => 8.51959031601596
+                ],
+                [
+                    "ds" => "2007-12-12",
+		 	        "y" => 8.18367658262066
+                ],
+                [
+                    "ds" => "2007-12-13",
+		 	        "y" => 8.07246736935477
+                ],
+                [
+                    "ds" => "2007-12-14",
+		 	        "y" => 7.8935720735049
+                ]
+            ]
+        ];
+
+        $forecasts = Http::post($forecast_endpoint, [$sales]) -> json();
+        return Inertia::render('analysis/Index', compact('forecasts'));
     }
 
     /**
