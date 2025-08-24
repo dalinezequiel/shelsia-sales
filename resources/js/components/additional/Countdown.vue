8e15cc3895ceac33b15a3b3a3efd5889047279fd
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-24 19:58:40 +0200

    refactor: add Product component

---
+++ resources/js/components/additional/Countdown.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+    <div class="rounded-lg inline-flex items-center">
+        <button
+            class="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4" />
+            </svg>
+        </button>
+        <div
+            class="bg-gray-100 border-t border-b border-gray-100 text-gray-600 hover:bg-gray-100 inline-flex items-center px-4 py-1 select-none">
+            2
+        </div>
+        <button
+            class="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+            </svg>
+        </button>
+    </div>
+</template>
--- resources/js/components/additional/Product.vue
+++ resources/js/components/additional/Product.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { ShoppingCart } from 'lucide-vue-next';
+import Countdown from './Countdown.vue';
 </script>
 <template>
-    <!-- Product Card 1 -->
     <div
         class="group relative bg-white rounded-xl shadow-lg overflow-hidden transition-all duration-500 hover:shadow-2xl hover:-translate-y-2">
         <div class="relative overflow-hidden h-43">
@@ -13,9 +13,10 @@ import { ShoppingCart } from 'lucide-vue-next';
                 class="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500">
             </div>
             <div
-                class="absolute bottom-0 left-0 right-0 p-4 translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500">
+                class="flex gap-2 absolute bottom-0 left-0 right-0 p-4 translate-y-10 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-500">
+                <Countdown />
                 <button
-                    class="w-full bg-indigo-600 text-white py-2 text-sm rounded-md font-medium cursor-pointer hover:bg-indigo-700 transition-colors duration-300 flex items-center justify-center gap-2">
+                    class="w-full bg-indigo-600 text-white px-0 text-sm rounded-sm font-medium cursor-pointer hover:bg-indigo-700 transition-colors duration-300 flex items-center justify-center gap-2">
                     <ShoppingCart width="16px" /> Add to Cart
                 </button>
             </div>
