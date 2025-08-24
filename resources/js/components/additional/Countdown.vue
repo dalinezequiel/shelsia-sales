ba9f8cb778679bcd295343fc8e75711aa86303a4
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-24 19:59:57 +0200

    refactor: add Countdown component

--- resources/js/components/additional/Countdown.vue
+++ resources/js/components/additional/Countdown.vue
@@ -3,7 +3,7 @@
 <template>
     <div class="rounded-lg inline-flex items-center">
         <button
-            class="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
+            class="bg-white rounded-l border text-gray-600 cursor-pointer hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H4" />
@@ -14,7 +14,7 @@
             2
         </div>
         <button
-            class="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
+            class="bg-white rounded-r border text-gray-600 cursor-pointer hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-4" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
