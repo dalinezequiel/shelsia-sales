83182596ddec65d8980c60d6e81a7e4c242835a0
dalinezequiel <dalinezequiel44@gmail.com>
2025-08-26 15:11:26 +0200

    feat: configure vuex

---
+++ resources/js/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+    state: {
+        product: {
+            cart: [],
+        },
+    },
+    mutations: {},
+    actions: {},
+    getters: {},
+});
