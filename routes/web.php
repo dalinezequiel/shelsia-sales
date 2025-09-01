42ba1de74110c911613c67c4241b3714deab13af
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-02 00:27:42 +0200

    add units route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Parameter\Supplier\SupplierController;
 use App\Http\Controllers\Parameter\Account\AccountController;
 use App\Http\Controllers\Parameter\PaymentMethod\PaymentMethod;
 use App\Http\Controllers\Parameter\ProductCategory\ProductCategory;
+use App\Http\Controllers\Parameter\Unit\UnitController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -29,7 +30,7 @@ Route::resource('analysis', AnalysisController::class);
 Route::prefix('parameters')->group(function () {
     Route::resource('/customers', CustomerController::class);
     Route::resource('/suppliers', SupplierController::class);
-    Route::resource('/accounts', AccountController::class);
+    Route::resource('/units', UnitController::class);
     Route::resource('/product_categories', ProductCategory::class);
     Route::resource('/payment_methods', PaymentMethod::class);
 });
