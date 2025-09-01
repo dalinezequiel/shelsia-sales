a5cf663cdc1691590f87c318c082d0abf2c8784b
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-01 13:27:03 +0200

    feat: implement edit method

--- app/Http/Controllers/Parameter/Supplier/SupplierController.php
+++ app/Http/Controllers/Parameter/Supplier/SupplierController.php
@@ -45,9 +45,9 @@ class SupplierController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Supplier $supplier)
     {
-        //
+        return Inertia::render('parameter/supplier/Edit', compact('supplier'));
     }
 
     /**
