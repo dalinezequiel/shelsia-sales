06249170a7d30fbef6d14e1941d84bd83be954e5
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-02 00:19:15 +0200

    feat: implement index method

---
+++ app/Http/Controllers/Parameter/Unit/UnitController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Parameter\Unit;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product\Unit;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $description = $request->query('description');
+        $units = Unit::where('description', 'like', '%' . $description . '%')->paginate(5);
+        return Inertia::render('parameter/unit/Index', compact('units'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
