10f5097913744a77f1220cb85ed26877d6bfcea3
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-01 10:28:19 +0200

    refactor: add product show page

--- resources/js/pages/product/Show.vue
+++ resources/js/pages/product/Show.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link } from '@inertiajs/vue3';
@@ -20,6 +21,19 @@ interface Product {
     is_active: boolean;
 }
 
+function printButton(elementId: string): void {
+    const printContent = document.getElementById(elementId);
+
+    if (printContent) {
+        const originalContent = document.body.innerHTML;
+        document.body.innerHTML = printContent.innerHTML;
+        window.print();
+        document.body.innerHTML = originalContent;
+    } else {
+        console.error(`Element with ID '${elementId}' not found.`);
+    }
+}
+
 const props = defineProps<{ product: Product }>();
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -38,51 +52,51 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Details" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="px-12">
-            <div class="max-w-4xl overflow-hidden bg-white sm:rounded-lg">
+            <div id="printableArea" class="max-w-4xl overflow-hidden bg-white sm:rounded-lg">
                 <div class="px-0 py-5 sm:px-6">
-                    <h3 class="text-lg leading-6 font-medium text-gray-900">Product details</h3>
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">Detalhes do Produto</h3>
                 </div>
                 <div class="border-t border-gray-200">
                     <dl>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Description</dt>
+                            <dt class="text-sm font-medium text-gray-500">Descrição</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.product.barcode
                                 }} - {{ props.product.description }}</dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Category</dt>
+                            <dt class="text-sm font-medium text-gray-500">Categoria</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.product.category }}
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Purchase price</dt>
+                            <dt class="text-sm font-medium text-gray-500">Preço de compra</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.product.purchase_price
                             }}
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Sale price</dt>
+                            <dt class="text-sm font-medium text-gray-500">Preço de venda</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.product.sale_price }}
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Available stock</dt>
+                            <dt class="text-sm font-medium text-gray-500">Estoque disponível</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{
                                 props.product.available_stock }}
                             </dd>
                         </div>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Supplier</dt>
+                            <dt class="text-sm font-medium text-gray-500">Fornecedor</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.product.supplier }}
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Validity</dt>
+                            <dt class="text-sm font-medium text-gray-500">Validade</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.product.validity
                                 }}</dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                            <dt class="text-sm font-medium text-gray-500">Status</dt>
+                            <dt class="text-sm font-medium text-gray-500">Situação</dt>
                             <dd v-if="props.product.is_active" class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                                 Activo</dd>
                             <dd v-else="props.product.is_active"
@@ -107,20 +121,21 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <path d="m12 19-7-7 7-7" />
                     <path d="M19 12H5" />
                 </svg>
-                Back
+                Voltar
                 </Link>
-                <Link
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-gray-900 text-white shadow-2xs hover:shadow-gray-900/20 disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden  dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
-                    href="#">
-                <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                    <polyline points="7 10 12 15 17 10" />
-                    <line x1="12" x2="12" y1="15" y2="3" />
-                </svg>
-                Report PDF
-                </Link>
+                <Button id="printButton" @click="printButton('printableArea')" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border cursor-pointer
+                    border-gray-200 bg-gray-900 text-white shadow-2xs hover:shadow-gray-900/20 disabled:opacity-50
+                    disabled:pointer-events-none focus:outline-hidden dark:border-neutral-700 dark:text-neutral-300
+                    dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
+                    <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                        <polyline points="7 10 12 15 17 10" />
+                        <line x1="12" x2="12" y1="15" y2="3" />
+                    </svg>
+                    Exportar PDF
+                </Button>
             </div>
         </div>
     </AppLayout>
