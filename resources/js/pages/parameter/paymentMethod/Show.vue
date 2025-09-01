cf70a0cb3f46199b473578975a6d161cfbb895c0
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-02 01:08:42 +0200

    refactor: add payment methods show page

--- resources/js/pages/parameter/paymentMethod/Show.vue
+++ resources/js/pages/parameter/paymentMethod/Show.vue
@@ -45,20 +45,21 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="px-12">
             <div id="printableArea" class="max-w-4xl overflow-hidden bg-white sm:rounded-lg">
                 <div class="px-0 py-5 sm:px-6">
-                    <h3 class="text-lg leading-6 font-medium text-gray-900">Detalhes de forma de pagar</h3>
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">Forma de {{ paymentMethod.operation_type }}
+                    </h3>
                 </div>
                 <div class="border-t border-gray-200">
                     <dl>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Descrição</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ paymentMethod.description
-                                }}</dd>
+                            }}</dd>
                         </div>
 
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Tipo de Operação</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ paymentMethod.operation_type
-                                }}
+                            }}
                             </dd>
                         </div>
 
