05dfd12626b2dbd1247c4b9282b99a070c9a3284
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-01 11:41:22 +0200

    refactor: add products index page

--- resources/js/pages/product/Index.vue
+++ resources/js/pages/product/Index.vue
@@ -86,7 +86,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <div class="flex flex-col gap-2 shrink-0 sm:flex-row">
                         <Link :href="route('products.index')"
                             class="flex gap-y-3 gap-x-2 select-none rounded-lg border border-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-gray-900 transition-all hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                        ver tudo
+                        Listar
                         </Link>
                         <button
                             class="flex select-none items-center gap-3 rounded-lg bg-gray-900 py-2 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
