46e3258a48184eada18bc3d6179d16553a32a800
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-02 00:08:22 +0200

    add Unit model

---
+++ app/Models/Product/Unit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Product;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Unit extends Model
+{
+    protected $fillable = [
+        'id',
+        'description',
+        'abbreviation',
+        'observation',
+        'is_active',
+        'created_at',
+    ];
+}
