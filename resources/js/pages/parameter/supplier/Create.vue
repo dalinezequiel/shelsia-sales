b3c8da88f19a545a34b73dc1c1db0994cf7654af
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-01 23:44:39 +0200

    refactor: add suppliers create page

--- resources/js/pages/parameter/supplier/Create.vue
+++ resources/js/pages/parameter/supplier/Create.vue
@@ -52,7 +52,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                         <div>
                             <h5
                                 class="text-blue-gray-900 block font-sans text-xl leading-snug font-semibold tracking-normal antialiased">
-                                Fornecedor
+                                Cadastrar Fornecedor
                             </h5>
                         </div>
                     </div>
