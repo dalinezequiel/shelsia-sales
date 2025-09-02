ad96cc2b44e9cf7823ac344d6e70489079481c4c
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-03 00:34:55 +0200

    refactor: add financial edit page

--- resources/js/pages/financial/Edit.vue
+++ resources/js/pages/financial/Edit.vue
@@ -7,6 +7,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import { PropType } from 'vue';
 import { toast } from 'vue-sonner';
 
 interface Account {
@@ -17,13 +18,30 @@ interface Account {
     due_date: Date;
     amount: number;
     date_of_issue: Date;
-    document_number: string;
+    payment_method: string;
     occurrence: string;
     observation: string;
     is_active: boolean;
 }
 
-const props = defineProps<{ financial: Account }>();
+const props = defineProps({
+    financial: {
+        type: Object as PropType<Account>,
+        required: true
+    },
+    suppliers: {
+        type: Object,
+        required: true
+    },
+    paymentMethods: {
+        type: Object,
+        required: true
+    },
+    periods: {
+        type: Object,
+        required: true
+    }
+});
 
 const form = useForm({
     account_plan: props.financial.account_plan,
@@ -33,7 +51,7 @@ const form = useForm({
     due_date: props.financial.due_date.toString(),
     amount: props.financial.amount,
     date_of_issue: props.financial.date_of_issue.toString(),
-    document_number: props.financial.document_number,
+    payment_method: props.financial.payment_method,
     occurrence: props.financial.occurrence,
     observation: props.financial.observation,
     is_active: Boolean(props.financial.is_active)
@@ -49,12 +67,12 @@ const submit = () => {
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Financial',
+        title: 'Financeiro',
         href: '/financial',
     },
     {
-        title: 'Registration',
-        href: '/financial/Create',
+        title: 'Actualização',
+        href: '/financial/Update',
     }
 ];
 </script>
@@ -69,7 +87,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <div>
                         <h5
                             class="text-blue-gray-900 block font-sans text-xl leading-snug font-semibold tracking-normal antialiased">
-                            Edit Account
+                            Editar Conta
                         </h5>
                     </div>
                 </div>
@@ -78,44 +96,44 @@ const breadcrumbs: BreadcrumbItem[] = [
                 <form @submit.prevent="submit" class="space-y-6">
                     <div class="grid lg:grid-cols-2 lg:gap-4 ">
                         <div class="grid gap-2 mb-6 lg:mb-0">
-                            <Label for="account_plan">Account plan</Label>
-                            <Input id="account_plan" v-model="form.account_plan" type="number" class="mt-1 block w-full"
-                                required placeholder="Account plan: x-xxx-xx-x" />
+                            <Label for="account_plan">Plano de contas</Label>
+                            <Input id="account_plan" v-model="form.account_plan" class="mt-1 block w-full" required
+                                placeholder="Plano de contas: x-xxx-xx-x" />
                             <InputError :message="form.errors.account_plan" class="mt-2" />
                         </div>
 
                         <div class="grid gap-2 mb-6 lg:mb-0">
-                            <Label for="description">Description</Label>
+                            <Label for="description">Descrição</Label>
                             <Input id="description" v-model="form.description" class="mt-1 block w-full" required
-                                autocomplete="description" placeholder="Description" />
+                                autocomplete="description" placeholder="Descrição" />
                             <InputError :message="form.errors.description" class="mt-2" />
                         </div>
                     </div>
 
                     <div class="grid lg:grid-cols-2 lg:gap-4 ">
                         <div class="grid gap-2 mb-6 lg:mb-0">
-                            <Label for="category">Category</Label>
+                            <Label for="category">Categoria</Label>
                             <Select id="category" v-model="form.category">
                                 <SelectTrigger class="w-auto mt-1">
-                                    <SelectValue placeholder="Select category" />
+                                    <SelectValue placeholder="Selecionar categoria" />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    <SelectItem value="Income"> Income </SelectItem>
-                                    <SelectItem value="Expense"> Expense </SelectItem>
+                                    <SelectItem value="Receitas"> Receitas </SelectItem>
+                                    <SelectItem value="Dispensas"> Despesas </SelectItem>
                                 </SelectContent>
                             </Select>
                             <InputError :message="form.errors.category" class="mt-2" />
                         </div>
 
                         <div class="grid gap-2">
-                            <Label for="supplier">Supplier</Label>
+                            <Label for="supplier">Fornecedor</Label>
                             <Select id="supplier" v-model="form.supplier">
                                 <SelectTrigger class="w-auto mt-1">
-                                    <SelectValue placeholder="Select supplier" />
+                                    <SelectValue placeholder="Selecionar fornecedor" />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    <SelectItem value="Handling Mozambique"> Handling Mozambique </SelectItem>
-                                    <SelectItem value="Wholesale Zimpeto Market"> Wholesale Zimpeto Market </SelectItem>
+                                    <SelectItem v-for="supplier in suppliers" :key="supplier.id" :value="supplier.name">
+                                        {{ supplier.name }}</SelectItem>
                                 </SelectContent>
                             </Select>
                             <InputError :message="form.errors.supplier" class="mt-2" />
@@ -124,67 +142,74 @@ const breadcrumbs: BreadcrumbItem[] = [
 
                     <div class="grid lg:grid-cols-3 lg:gap-4 ">
                         <div class="grid gap-2">
-                            <Label for="due_date">Due date</Label>
+                            <Label for="due_date">Dta de vencimento</Label>
                             <Input id="due_date" v-model="form.due_date" type="date" class="mt-1 block w-full" required
-                                placeholder="Due date" />
+                                placeholder="Data de vencimento" />
                             <InputError :message="form.errors.due_date" class="mt-2" />
                         </div>
 
                         <div class="grid gap-2 mb-6 lg:mb-0">
-                            <Label for="amount">Amount</Label>
+                            <Label for="amount">Valor da conta</Label>
                             <Input id="amount" v-model="form.amount" type="number" class="mt-1 block w-full" required
-                                placeholder="Amount" />
+                                placeholder="Valor da conta" />
                             <InputError :message="form.errors.amount" class="mt-2" />
                         </div>
                     </div>
 
                     <div class="grid lg:grid-cols-3 lg:gap-4 ">
                         <div class="grid gap-2">
-                            <Label for="date_of_issue">Date of issue</Label>
+                            <Label for="date_of_issue">Data de emissão</Label>
                             <Input id="date_of_issue" v-model="form.date_of_issue" type="date" class="mt-1 block w-full"
-                                required placeholder="Validity" />
+                                required placeholder="Data de emissão" />
                             <InputError :message="form.errors.date_of_issue" class="mt-2" />
                         </div>
 
-                        <div class="grid gap-2 mb-6 lg:mb-0">
-                            <Label for="document_number">Document number</Label>
-                            <Input id="document_number" v-model="form.document_number" type="number"
-                                class="mt-1 block w-full" required placeholder="Document number" />
-                            <InputError :message="form.errors.document_number" class="mt-2" />
+                        <div class="grid gap-2">
+                            <Label for="supplier">Forma de pagamento</Label>
+                            <Select id="supplier" v-model="form.payment_method">
+                                <SelectTrigger class="w-auto mt-1">
+                                    <SelectValue placeholder="Selecionar forma de pagamento" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem v-for="paymentMethod in paymentMethods" :key="paymentMethod.id"
+                                        :value="paymentMethod.description">{{ paymentMethod.description }}</SelectItem>
+                                </SelectContent>
+                            </Select>
+                            <InputError :message="form.errors.supplier" class="mt-2" />
                         </div>
 
                         <div class="grid gap-2">
-                            <Label for="occurrence">Occurrence</Label>
+                            <Label for="occurrence">Periodicidade</Label>
                             <Select id="occurrence" v-model="form.occurrence">
                                 <SelectTrigger class="w-auto mt-1">
-                                    <SelectValue placeholder="Select occurrence" />
+                                    <SelectValue placeholder="Selecionar periodicidade" />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    <SelectItem value="Monthly"> Monthly </SelectItem>
-                                    <SelectItem value="Semi-Annual"> Semi-Annual </SelectItem>
-                                    <SelectItem value="Annual"> Annual </SelectItem>
+                                    <SelectItem v-for="period in periods" :key="period.id" :value="period.description">
+                                        {{ period.description }}</SelectItem>
                                 </SelectContent>
                             </Select>
                             <InputError :message="form.errors.occurrence" class="mt-2" />
                         </div>
 
                         <div class="grid gap-2 mb-6 lg:mb-0">
-                            <Label for="observation">Observation</Label>
+                            <Label for="observation">Observação</Label>
                             <textarea id="observation" rows="4" v-model="form.observation"
                                 class="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Write your observation here..."></textarea>
+                                placeholder="Escreva sua observação aqui..."></textarea>
                             <InputError :message="form.errors.observation" class="mt-2" />
                         </div>
                     </div>
 
                     <div class="flex items-center space-x-2 ">
                         <label class="inline-flex items-center cursor-pointer">
-                            <input type="checkbox" id="is_active" v-model="form.is_active" class="sr-only peer">
+                            <input type="checkbox" id="is_active" v-model="form.is_active" disabled
+                                class="sr-only peer">
                             <div
                                 class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-gray-900 dark:peer-checked:bg-blue-600">
                             </div>
-                            <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Account
-                                status</span>
+                            <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Pagamento
+                                efectuado</span>
                         </label>
                     </div>
 
@@ -197,17 +222,15 @@ const breadcrumbs: BreadcrumbItem[] = [
                             <path d="m12 19-7-7 7-7" />
                             <path d="M19 12H5" />
                         </svg>
-                        Back</Link>
+                        Voltar</Link>
                         <Button class="cursor-pointer">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="lucide lucide-save-icon lucide-save">
-                                <path
-                                    d="M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z" />
-                                <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7" />
-                                <path d="M7 3v4a1 1 0 0 0 1 1h7" />
+                                stroke-linejoin="round" class="lucide lucide-rotate-ccw-icon lucide-rotate-ccw">
+                                <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+                                <path d="M3 3v5h5" />
                             </svg>
-                            Save Changes</Button>
+                            Actualizar</Button>
                     </div>
                 </form>
             </div>
