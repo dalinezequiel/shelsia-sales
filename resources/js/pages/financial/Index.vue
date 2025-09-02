fade0bbe5ce7e493c309f8260d20c8040b55e990
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-02 17:03:29 +0200

    feat: add Pagination component

--- resources/js/pages/financial/Index.vue
+++ resources/js/pages/financial/Index.vue
@@ -20,6 +20,7 @@ import {
 import { toast } from 'vue-sonner';
 import { ref } from 'vue';
 import Tablist from '@/components/additional/Tablist.vue';
+import Pagination from '@/components/additional/Pagination.vue';
 
 defineProps({
     accounts: {
@@ -336,33 +337,8 @@ const breadcrumbs: BreadcrumbItem[] = [
                     </tbody>
                 </table>
             </div>
-            <div class="flex justify-between items-center px-4 py-4">
-                <div class="text-sm text-slate-500">
-                    Showing <b>1-5</b> of 45
-                </div>
-                <div class="flex space-x-1">
-                    <button
-                        class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-                        Prev
-                    </button>
-                    <button
-                        class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-primary border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
-                        1
-                    </button>
-                    <button
-                        class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-                        2
-                    </button>
-                    <button
-                        class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-                        3
-                    </button>
-                    <button
-                        class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
-                        Next
-                    </button>
-                </div>
-            </div>
+
+            <Pagination :list="accounts" />
         </div>
     </AppLayout>
 </template>
