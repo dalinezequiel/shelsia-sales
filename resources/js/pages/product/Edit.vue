8cb2e8efb40f8d2b17810dd73e1f91b7c17c9aed
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-05 11:00:19 +0200

    refactor: add products edit page

--- resources/js/pages/product/Edit.vue
+++ resources/js/pages/product/Edit.vue
@@ -9,6 +9,7 @@ import { type BreadcrumbItem } from '@/types';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 import { PropType } from 'vue';
 import { toast } from 'vue-sonner';
+import { Loader } from 'lucide-vue-next';
 
 interface Product {
     barcode: number;
@@ -236,13 +237,15 @@ const breadcrumbs: BreadcrumbItem[] = [
                         </svg>
                         Voltar</Link>
                         <Button class="cursor-pointer">
+                            <Loader class="animate-spin" v-if="form.processing" />
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="lucide lucide-rotate-ccw-icon lucide-rotate-ccw">
                                 <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
                                 <path d="M3 3v5h5" />
                             </svg>
-                            Actualizar</Button>
+                            Actualizar
+                        </Button>
                     </div>
                 </form>
             </div>
