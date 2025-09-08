a598f9a194514596695cb93301782154e0798c99
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-08 15:50:26 +0200

    refactor: add sales index page

--- resources/js/pages/sales/Index.vue
+++ resources/js/pages/sales/Index.vue
@@ -209,9 +209,11 @@ const breadcrumbs: BreadcrumbItem[] = [
                             </td>
                             <td class="p-4 border-b border-blue-gray-50">
                                 <div class="w-max">
-                                    <div
+                                    <div :class="sale.status ? 'bg-green-500/20' : (sale.status == 'pending' ? 'bg-amber-500/20' : 'bg-red-500/20')"
                                         class="relative grid items-center px-2 py-1 font-sans text-xs font-bold text-green-900 uppercase rounded-md select-none whitespace-nowrap bg-green-500/20">
-                                        <span class="">paid</span>
+                                        <span v-if="sale.status == 'paid'" class="">Pago</span>
+                                        <span v-else-if="sale.status == 'pending'" class="text-red-900">Pendente</span>
+                                        <span v-else class="text-red-900">Cancelado</span>
                                     </div>
                                 </div>
                             </td>
