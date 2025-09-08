5665a10ffa07e4bba1ee9fa840d5252d94b9abf7
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-08 23:39:01 +0200

    refactor: add sales index page

--- resources/js/pages/sales/Index.vue
+++ resources/js/pages/sales/Index.vue
@@ -321,6 +321,14 @@ const breadcrumbs: BreadcrumbItem[] = [
                                         </AlertDialogHeader>
                                         <AlertDialogFooter>
                                             <AlertDialogCancel class="cursor-pointer">Cancelar</AlertDialogCancel>
+                                            <AlertDialogAction
+                                                class="cursor-pointer  border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                                                    <path
+                                                        d="M530.8 134.1C545.1 144.5 548.3 164.5 537.9 178.8L281.9 530.8C276.4 538.4 267.9 543.1 258.5 543.9C249.1 544.7 240 541.2 233.4 534.6L105.4 406.6C92.9 394.1 92.9 373.8 105.4 361.3C117.9 348.8 138.2 348.8 150.7 361.3L252.2 462.8L486.2 141.1C496.6 126.8 516.6 123.6 530.9 134z" />
+                                                </svg>
+                                                Pagar
+                                            </AlertDialogAction>
                                             <AlertDialogAction @click="updateSale(sale.id, sale.has_details)"
                                                 class="cursor-pointer bg-[#EC3636] hover:bg-[#D4563F]">
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"
