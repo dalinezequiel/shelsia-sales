cc37d7e51a995400f8753ae30546a42f59531bdf
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-09 12:59:22 +0200

    refactor: implement store method

--- app/Http/Controllers/Sales/SalesController.php
+++ app/Http/Controllers/Sales/SalesController.php
@@ -60,7 +60,8 @@ class SalesController extends Controller
                 'customer_name' => $request->customer_name,
                 'discount' => $request->discount,
                 'shipping' => $request->shipping,
-                'payment_method' => $request->payment_method
+                'payment_method' => $request->payment_method,
+                'status' => $request->status
             ]);
 
             $items = [];
