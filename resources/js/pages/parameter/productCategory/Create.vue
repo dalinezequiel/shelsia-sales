c34516eb03bab6b5247e4e6c93d70879842f7a7a
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-12 14:26:30 +0200

    refactor: add productCategories create page

--- resources/js/pages/parameter/productCategory/Create.vue
+++ resources/js/pages/parameter/productCategory/Create.vue
@@ -9,6 +9,7 @@ import ParameterLayout from '@/pages/parameter/Layout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 import { toast } from 'vue-sonner';
+import { Loader } from 'lucide-vue-next';
 
 const form = useForm({
     description: '',
@@ -47,7 +48,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <div class="mb-8 flex items-center justify-between gap-8">
                         <div>
                             <h5
-                                class="text-blue-gray-900 block font-sans text-xl leading-snug font-semibold tracking-normal antialiased">
+                                class="text-blue-gray-900 block font-sans text-lg leading-snug font-semibold tracking-normal antialiased">
                                 Categoria de Produto
                             </h5>
                         </div>
@@ -90,6 +91,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                             </svg>
                             Voltar</Link>
                             <Button class="cursor-pointer">
+                                <Loader class="animate-spin" v-if="form.processing" />
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                     stroke-linejoin="round" class="lucide lucide-save-icon lucide-save">
@@ -98,7 +100,8 @@ const breadcrumbs: BreadcrumbItem[] = [
                                     <path d="M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7" />
                                     <path d="M7 3v4a1 1 0 0 0 1 1h7" />
                                 </svg>
-                                Salvar</Button>
+                                Salvar
+                            </Button>
                         </div>
                     </form>
                 </div>
