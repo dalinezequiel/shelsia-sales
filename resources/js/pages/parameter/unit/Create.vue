015ff5c5b28019e996262445f7738c7ce5aae20f
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-12 14:16:39 +0200

    refactor: add units create page

--- resources/js/pages/parameter/unit/Create.vue
+++ resources/js/pages/parameter/unit/Create.vue
@@ -48,7 +48,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <div class="mb-8 flex items-center justify-between gap-8">
                         <div>
                             <h5
-                                class="text-blue-gray-900 block font-sans text-xl leading-snug font-semibold tracking-normal antialiased">
+                                class="text-blue-gray-900 block font-sans text-lg leading-snug font-semibold tracking-normal antialiased">
                                 Unidade de Medida
                             </h5>
                         </div>
