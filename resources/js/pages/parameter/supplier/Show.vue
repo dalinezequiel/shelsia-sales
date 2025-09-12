7544d8c04b810d3a2e113f67e4fac41e09916aee
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-12 15:19:56 +0200

    refactor: add suppliers show page

--- resources/js/pages/parameter/supplier/Show.vue
+++ resources/js/pages/parameter/supplier/Show.vue
@@ -14,6 +14,9 @@ interface Supplier {
     is_active: boolean;
 }
 
+const gender = (gender: string) => gender === 'male' ? 'Masculino'
+    : (gender === 'female' ? 'Feminino' : 'Indefinido')
+
 function printButton(elementId: string): void {
     const printContent = document.getElementById(elementId);
 
@@ -54,7 +57,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Nome</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.supplier.name
-                            }}</dd>
+                                }}</dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Apelido</dt>
@@ -63,7 +66,8 @@ const breadcrumbs: BreadcrumbItem[] = [
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Gênero</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.supplier.gender
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{
+                                gender(props.supplier.gender)
                                 }}
                             </dd>
                         </div>
