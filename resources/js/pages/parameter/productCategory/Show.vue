684640682e24188cd31ab539d28ad859a70f113a
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-12 15:14:23 +0200

    refactor: add productCategories show page

--- resources/js/pages/parameter/productCategory/Show.vue
+++ resources/js/pages/parameter/productCategory/Show.vue
@@ -44,20 +44,20 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="px-12">
             <div id="printableArea" class="max-w-4xl overflow-hidden bg-white sm:rounded-lg">
                 <div class="px-0 py-5 sm:px-6">
-                    <h3 class="text-lg leading-6 font-medium text-gray-900">Categoria de produto</h3>
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">Categoria de Produto</h3>
                 </div>
                 <div class="border-t border-gray-200">
                     <dl>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Descrição</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ productCategory.description
-                            }}</dd>
+                                }}</dd>
                         </div>
 
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Observação</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ productCategory.observation
-                                }}
+                            }}
                             </dd>
                         </div>
 
