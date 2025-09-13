c86b714e07fcfc8bf777d8de46b04833040ce54a
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-13 14:22:01 +0200

    feat: add create_finances_table migration

---
+++ database/migrations/2025_09_13_121043_create_finances_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('finances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('supplier_id')
+                ->constrained()
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
+            $table->foreignId('payment_method_id')
+                ->constrained()
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
+            $table->foreignId('period_id')
+                ->constrained()
+                ->onDelete('CASCADE')
+                ->onUpdate('CASCADE');
+            $table->string('account_plan');
+            $table->string('description')->unique();
+            $table->enum('category', ['expense', 'income'])->default('expense');
+            $table->date('due_date');
+            $table->decimal('amount', '10', '2');
+            $table->date('date_of_issue');
+            $table->string('observation')->nullable();
+            $table->boolean('is_active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('finances');
+    }
+};
