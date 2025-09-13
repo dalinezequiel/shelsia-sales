9ac0bce14191c5f152a7f23d5472396ced0ee33c
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-13 16:16:53 +0200

    refactor: add finances show page

--- resources/js/pages/finance/Show.vue
+++ resources/js/pages/finance/Show.vue
@@ -12,12 +12,12 @@ interface Account {
     amount: number;
     date_of_issue: Date;
     payment_method: string;
-    occurrence: string;
+    period: string;
     observation: string;
     is_active: boolean;
 }
 
-const props = defineProps<{ financial: Account }>();
+const props = defineProps<{ finance: Account }>();
 
 function printButton(elementId: string): void {
     const printContent = document.getElementById(elementId);
@@ -51,57 +51,58 @@ const breadcrumbs: BreadcrumbItem[] = [
             <div id="printableArea" class="max-w-4xl overflow-hidden bg-white sm:rounded-lg">
                 <div class="px-0 py-5 sm:px-6">
                     <h3 class="text-lg leading-6 font-medium text-gray-900">Conta a {{
-                        props.financial.category === 'Receitas' ? 'Pagar' : 'Receber' }}</h3>
+                        props.finance.category === 'Receitas' ? 'Pagar' : 'Receber' }}</h3>
                 </div>
                 <div class="border-t border-gray-200">
                     <dl>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Descrição</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.financial.account_plan
-                            }} - {{ props.financial.description }}</dd>
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.finance.account_plan
+                            }} - {{ props.finance.description }}</dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Valor da conta</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.financial.amount }}
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.finance.amount }}
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Data de vencimento</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.financial.due_date }}
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.finance.due_date }}
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Forma de pagamento</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{
-                                props.financial.payment_method }}
+                                props.finance.payment_method }}
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Categoria</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.financial.category }}
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.finance.category ===
+                                'income' ? 'Receitas' : 'Despesas' }}
                             </dd>
                         </div>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Fornecedor</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.financial.supplier }}
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.finance.supplier }}
                             </dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Periodicidade</dt>
-                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.financial.occurrence
+                            <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{ props.finance.period
                             }}</dd>
                         </div>
                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Estado da conta</dt>
-                            <dd v-if="props.financial.is_active"
-                                class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Pago</dd>
+                            <dd v-if="props.finance.is_active" class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                                Pago</dd>
                             <dd v-else="props.financial.is_active"
                                 class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Não Pago</dd>
                         </div>
                         <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                             <dt class="text-sm font-medium text-gray-500">Observação</dt>
                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                                {{ props.financial.observation }}
+                                {{ props.finance.observation }}
                             </dd>
                         </div>
                     </dl>
@@ -109,7 +110,7 @@ const breadcrumbs: BreadcrumbItem[] = [
             </div>
 
             <div class="mt-6 flex cursor-pointer justify-start gap-x-3">
-                <Link :href="route('financial.index')"
+                <Link :href="route('finances.index')"
                     class="inline-flex items-center gap-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-800 shadow-2xs hover:bg-gray-50 focus:bg-gray-50 focus:outline-hidden disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-transparent dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
