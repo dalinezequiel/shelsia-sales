f3d24e963b9b95f4aa70079c9f5ae66d03e1f9d6
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-14 16:51:07 +0200

    refactor: add SaleStatus component

--- resources/js/components/additional/SaleStatus.vue
+++ resources/js/components/additional/SaleStatus.vue
@@ -5,22 +5,27 @@
             <thead>
                 <tr>
                     <th
-                        class="w-2/5 px-6 bg-gray-50 text-gray-700 align-middle py-3 text-xs font-semibold text-left border-l-0 border-r-0 whitespace-nowrap">
+                        class="w-2/5 px-6 text-gray-700 align-middle py-3 text-md font-semibold text-left border-l-0 border-r-0 whitespace-nowrap">
                         Vendas Diárias</th>
-                    <th
-                        class="px-4 bg-gray-50 text-gray-700 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
-                        Núm.</th>
-                    <th
-                        class="w-40 px-4 bg-gray-50 text-gray-700 text-start py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
-                        Percentual
-                    </th>
-                    <th
-                        class="px-6 bg-gray-50 text-gray-700 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap min-w-140-px">
-                        Montante
-                    </th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-100">
+                <tr>
+                    <th
+                        class="w-2/5 px-6 bg-gray-50 text-gray-500 align-middle py-3 text-xs font-semibold text-left border-l-0 border-r-0 whitespace-nowrap">
+                        Situação</th>
+                    <th
+                        class="px-4 bg-gray-50 text-gray-500 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
+                        Núm.</th>
+                    <th
+                        class="w-40 px-4 bg-gray-50 text-gray-500 text-start py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
+                        Percentual
+                    </th>
+                    <th
+                        class="px-6 bg-gray-50 text-gray-500 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap min-w-140-px">
+                        Montante
+                    </th>
+                </tr>
                 <tr class="text-gray-500">
                     <th class="border-t-0 px-6 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">
                         Pagas</th>
