09386057bba5ae3dd5453295434c4f98451b782c
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-16 02:28:24 +0200

    refactor: add SaleStatus component

--- resources/js/components/additional/SaleStatus.vue
+++ resources/js/components/additional/SaleStatus.vue
@@ -36,33 +36,41 @@ const each_sale_total = (shipping: number, discount: number, item: any) => {
             <thead>
                 <tr>
                     <th
-                        class="w-2/5 px-6 text-gray-700 align-middle py-3 text-md font-semibold text-left border-l-0 border-r-0 whitespace-nowrap">
+                        class="px-6 text-gray-700 align-middle py-3 text-md font-semibold text-left border-l-0 border-r-0 whitespace-nowrap">
                         Vendas Diárias</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-100">
-                <tr>
-                    <th
-                        class="w-2/5 px-6 bg-gray-50 text-gray-500 align-middle py-3 text-xs font-semibold text-left border-l-0 border-r-0 whitespace-nowrap">
-                        Situação</th>
-                    <th
-                        class="px-4 bg-gray-50 text-gray-500 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
-                        Núm.</th>
-                    <th
-                        class="w-40 px-4 bg-gray-50 text-gray-500 text-start py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
+                <tr class="grid grid-cols-5">
+                    <td
+                        class="px-6 bg-gray-50 text-gray-500 align-middle py-3 text-xs font-semibold text-left border-l-0 border-r-0 whitespace-nowrap">
+                        Situação</td>
+
+                    <td
+                        class="px-3 bg-gray-50 text-gray-500 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
+                        Progresso</td>
+                    <td
+                        class="px-3 bg-gray-50 text-gray-500 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
+                        Núm.</td>
+                    <td
+                        class="px-4 bg-gray-50 text-gray-500 text-start py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
                         Percentual
-                    </th>
-                    <th
-                        class="px-6 bg-gray-50 text-gray-500 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap min-w-140-px">
+                    </td>
+                    <td
+                        class="px-6 bg-gray-50 text-gray-500 text-end py-3 text-xs font-semibold border-l-0 border-r-0 whitespace-nowrap">
                         Montante
-                    </th>
+                    </td>
                 </tr>
-                <tr class="text-gray-500">
-                    <th class="border-t-0 px-6 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">
-                        Pagas</th>
-                    <td class="border-t-0 px-4 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                <tr class="grid grid-cols-5 py-4 text-gray-500">
+                    <td class="border-t-0 px-6 text-sm font-normal whitespace-nowrap">
+                        Pagas</td>
+                    <td class="border-t-0 px-4 text-end text-xs font-medium whitespace-nowrap">
+                        <span class="px-2 py-1 rounded-md border border-gray-200">0 / 0</span>
+                    </td>
+
+                    <td class="border-t-0 px-4 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         {{ indicators.sales.paid.total }}</td>
-                    <td class="w-40 border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
+                    <td class="w-40 border-t-0 px-4 align-middle text-xs whitespace-nowrap">
                         <div class="flex items-center">
                             <span class="mr-2 text-xs font-medium">{{ percents(indicators.sales.total,
                                 indicators.sales.paid.total).toFixed(0) }}%</span>
@@ -76,19 +84,22 @@ const each_sale_total = (shipping: number, discount: number, item: any) => {
                         </div>
                     </td>
                     <td v-if="indicators.sales.paid.items_to_sum"
-                        class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                        class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         {{ calculate(indicators.sales.paid.items_to_sum) }}
                     </td>
-                    <td v-else class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                    <td v-else class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         0,00
                     </td>
                 </tr>
-                <tr class="text-gray-500">
-                    <th class="border-t-0 px-6 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">
-                        Pendentes</th>
-                    <td class="border-t-0 px-4 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                <tr class="grid grid-cols-5 py-4 text-gray-500">
+                    <td class="border-t-0 px-6 text-sm font-normal whitespace-nowrap">
+                        Pendentes</td>
+                    <td class="border-t-0 px-4 text-end text-xs font-medium whitespace-nowrap">
+                        <span class="px-2 py-1 rounded-md border border-gray-200">0 / 0</span>
+                    </td>
+                    <td class="border-t-0 px-4 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         {{ indicators.sales.pending.total }}</td>
-                    <td class="w-40 border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
+                    <td class="w-40 border-t-0 px-4 text-xs whitespace-nowrap">
                         <div class="flex items-center">
                             <span class="mr-2 text-xs font-medium">{{ percents(indicators.sales.total,
                                 indicators.sales.pending.total).toFixed(0) }}%</span>
@@ -102,20 +113,23 @@ const each_sale_total = (shipping: number, discount: number, item: any) => {
                         </div>
                     </td>
                     <td v-if="indicators.sales.pending.items_to_sum"
-                        class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                        class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         {{ calculate(indicators.sales.pending.items_to_sum) }}
                     </td>
-                    <td v-else class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                    <td v-else class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         0,00
                     </td>
                 </tr>
-                <tr class="text-gray-500">
-                    <th class="border-t-0 px-6 align-middle text-sm font-normal whitespace-nowrap p-4 text-left">
+                <tr class="grid grid-cols-5 py-4 text-gray-500">
+                    <td class="border-t-0 px-6 align-middle text-sm font-normal whitespace-nowrap">
                         Canceladas
-                    </th>
-                    <td class="border-t-0 px-4 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                    </td>
+                    <td class="border-t-0 px-4 text-end text-xs font-medium  whitespace-nowrap">
+                        <span class="px-2 py-1 rounded-md border border-gray-200">0 / 0</span>
+                    </td>
+                    <td class="border-t-0 px-4 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         {{ indicators.sales.cancelled.total }}</td>
-                    <td class="w-40 border-t-0 px-4 align-middle text-xs whitespace-nowrap p-4">
+                    <td class="w-40 border-t-0 px-4 text-xs whitespace-nowrap">
                         <div class="flex items-center">
                             <span class="mr-2 text-xs font-medium">{{ percents(indicators.sales.total,
                                 indicators.sales.cancelled.total).toFixed(0) }}%</span>
@@ -130,10 +144,10 @@ const each_sale_total = (shipping: number, discount: number, item: any) => {
                     </td>
 
                     <td v-if="indicators.sales.cancelled.items_to_sum"
-                        class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                        class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         {{ calculate(indicators.sales.cancelled.items_to_sum) }}
                     </td>
-                    <td v-else class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap p-4">
+                    <td v-else class="border-t-0 px-6 text-end text-xs font-medium text-gray-900 whitespace-nowrap">
                         0,00
                     </td>
                 </tr>
