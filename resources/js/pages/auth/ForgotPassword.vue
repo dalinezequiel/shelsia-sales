ff783104802977ed8856bf4203e071161c31c049
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-16 16:47:06 +0200

    refactor: add ForgotPassword page

--- resources/js/pages/auth/ForgotPassword.vue
+++ resources/js/pages/auth/ForgotPassword.vue
@@ -22,8 +22,10 @@ const submit = () => {
 </script>
 
 <template>
-    <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
-        <Head title="Forgot password" />
+    <AuthLayout title="Esqueceu sua senha"
+        description="Digite seu e-mail para receber um link para redefinir sua senha">
+
+        <Head title="Esqueceu sua senha" />
 
         <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
             {{ status }}
@@ -32,22 +34,23 @@ const submit = () => {
         <div class="space-y-6">
             <form @submit.prevent="submit">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
-                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus placeholder="email@example.com" />
+                    <Label for="email">Email</Label>
+                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus
+                        placeholder="email@exemplo.com" />
                     <InputError :message="form.errors.email" />
                 </div>
 
                 <div class="my-6 flex items-center justify-start">
-                    <Button class="w-full" :disabled="form.processing">
+                    <Button class="w-full cursor-pointer" :disabled="form.processing">
                         <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                        Email password reset link
+                        Link de redefinição de senha de e-mail
                     </Button>
                 </div>
             </form>
 
             <div class="space-x-1 text-center text-sm text-muted-foreground">
-                <span>Or, return to</span>
-                <TextLink :href="route('login')">log in</TextLink>
+                <span>Ou, volte para</span>
+                <TextLink :href="route('login')">login</TextLink>
             </div>
         </div>
     </AuthLayout>
