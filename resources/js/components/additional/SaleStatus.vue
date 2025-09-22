b2d7e962b8c733e199ce7e399e2208a5cb30f00b
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-22 12:30:33 +0200

    fix: The percentages method returned zero when the result was not different from an integer

--- resources/js/components/additional/SaleStatus.vue
+++ resources/js/components/additional/SaleStatus.vue
@@ -9,7 +9,7 @@ defineProps({
 });
 
 const percents = (total: number, total_per_situation: number) =>
-    Number.isInteger((total_per_situation * 100) / total) ? (total_per_situation * 100) / total : 0;
+    Number.isInteger(total_per_situation) ? (total_per_situation * 100) / total : 0;
 const applyProgressBar = (number: number) => "width: ".concat(number.toString(), "%");
 
 const calculate = (items: any) => {
