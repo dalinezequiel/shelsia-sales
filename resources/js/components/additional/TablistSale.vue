8e859ae750093113f55e2d7711a04d7890888c83
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-24 11:09:59 +0200

    feat: add TablistSale component

---
+++ resources/js/components/additional/TablistSale.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+defineProps({
+    list: {
+        type: Object,
+        required: true
+    }
+});
+
+</script>
+<template>
+    <section>
+        <ul class="flex">
+            <li>
+                <button
+                    class="focus:outline-none p-2 rounded-l-lg border border-r-0 bg-white flex space-x-1 items-center w-24">
+                    <p class="font-semibold text-md">{{ list.paid + list.pending + list.cancelled }}</p>
+                    <p class="text-gray-600 text-sm">
+                        Contas
+                    </p>
+                </button>
+            </li>
+            <li>
+                <button class="focus:outline-none p-2 border border-r-0 bg-white flex space-x-1 items-center w-24">
+                    <p class="font-semibold text-md">{{ list.paid }}</p>
+                    <p class="text-gray-600 text-sm">
+                        Pagas
+                    </p>
+                </button>
+            </li>
+            <li>
+                <button class="focus:outline-none p-2 border border-r-0 bg-white flex space-x-1 items-center w-24">
+                    <p class="font-semibold text-md">{{ list.pending }}</p>
+                    <p class="text-gray-600 text-sm">
+                        Pendentes
+                    </p>
+                </button>
+            </li>
+            <li>
+                <button class="focus:outline-none p-2 border rounded-r-lg bg-white flex space-x-1 items-center w-26">
+                    <p class="font-semibold text-md">{{ list.cancelled }}</p>
+                    <p class="text-gray-600 text-sm">
+                        Canceladas
+                    </p>
+                </button>
+            </li>
+        </ul>
+    </section>
+</template>
