6d8e665a35fa5ceaff9af866bfe213a6350c1768
dalinezequiel <dalinezequiel44@gmail.com>
2025-09-25 22:41:50 +0200

    refactor: rename page title to dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -15,7 +15,7 @@ defineProps({
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboard',
+        title: 'Painel',
         href: '/dashboard',
     },
 ];
@@ -23,7 +23,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
 
-    <Head title="Dashboard" />
+    <Head title="Painel" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
